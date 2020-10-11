88cc5b9644fc7f7351eca14ed07013782caac8af
Mathews Maloba <mathewsmaloba88@gmail.com>
2020-10-11 18:29:10 +0300

    Add files via upload
    Simple Use state in React JS to map in an array of Data

---
+++ 3-useState-array.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { data } from '../../../data';
+
+const UseStateArray = () => {
+
+const [people,setPeople]=React.useState(data);
+
+  return(
+<>
+{people.map((person)=>{
+  const {id,name}=person;
+return<div className="item" key={id}>{}
+<h4>{name}</h4>
+</div>
+})}
+<button className='btn' onClick={()=>setPeople([])}>Clear</button>
+</>
+  );
+};
+
+export default UseStateArray;
